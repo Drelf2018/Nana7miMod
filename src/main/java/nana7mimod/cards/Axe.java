d6df6a93f93f218843ae60f95b0fcc783e012d27
12138 <drelf2018@outlook.com>
2025-08-13 00:01:22 +0800

    feat: 收起斧头

---
+++ src/main/java/nana7mimod/cards/Axe.java
@@ -0,0 +1,37 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.ATFieldPower;
+
+public class Axe extends Base {
+    public static final String ID = ModHelper.id(Axe.class);
+
+    public Axe() {
+        super(ID, CardCost.C2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        this.magicNumber = this.baseMagicNumber = 2;
+        this.exhaust = true;
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int discardCount = 0;
+        for (AbstractCard c : p.hand.group) {
+            if (c.type == CardType.ATTACK) {
+                addToBot(new DiscardSpecificCardAction(c, p.hand));
+                discardCount++;
+            }
+        }
+        addToBot(new ApplyPowerAction(p, p, new ATFieldPower(p, discardCount * magicNumber)));
+    }
+}
