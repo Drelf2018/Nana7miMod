a5bb956004b7130e22ba530732c2599a31e19ba6
12138 <drelf2018@outlook.com>
2025-08-21 22:48:43 +0800

    fix: 美化养了挠挠

--- src/main/java/nana7mimod/cards/Scratch.java
+++ src/main/java/nana7mimod/cards/Scratch.java
@@ -1,11 +1,16 @@
 package nana7mimod.cards;
 
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ClawEffect;
+import nana7mimod.helpers.CharacterHelper.Nana7mi;
+import nana7mimod.effects.ClawEffectMirror;
 import nana7mimod.helpers.ModHelper;
 
 public class Scratch extends Base {
@@ -27,8 +32,10 @@ public class Scratch extends Base {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         for (int i = 0; i < magicNumber / 2; i++) {
-            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.SLASH_HORIZONTAL));
-            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.SLASH_VERTICAL));
+            addToBot(new VFXAction(new ClawEffect(m.hb.cX, m.hb.cY, Nana7mi.COLOR, Color.WHITE), 0.1F));
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.NONE));
+            addToBot(new VFXAction(new ClawEffectMirror(m.hb.cX, m.hb.cY, Nana7mi.COLOR, Color.WHITE), 0.1F));
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.NONE));
         }
     }
 }
---
+++ src/main/java/nana7mimod/effects/ClawEffectMirror.java
@@ -0,0 +1,42 @@
+package nana7mimod.effects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.AnimatedSlashEffect;
+
+public class ClawEffectMirror extends AbstractGameEffect {
+    private float x;
+
+    private float y;
+
+    private Color color2;
+
+    public ClawEffectMirror(float x, float y, Color color1, Color color2) {
+        this.x = x;
+        this.y = y;
+        this.color = color1;
+        this.color2 = color2;
+        this.startingDuration = 0.1F;
+        this.duration = this.startingDuration;
+    }
+
+    public void update() {
+        if (MathUtils.randomBoolean()) {
+            CardCrawlGame.sound.playA("ATTACK_DAGGER_5", MathUtils.random(0.0F, -0.3F));
+        } else {
+            CardCrawlGame.sound.playA("ATTACK_DAGGER_6", MathUtils.random(0.0F, -0.3F));
+        }
+        AbstractDungeon.effectsQueue.add(new AnimatedSlashEffect(this.x - 35.0F, this.y + 35.0F, -150.0F, -150.0F, -225.0F, this.color, this.color2));
+        AbstractDungeon.effectsQueue.add(new AnimatedSlashEffect(this.x, this.y, -150.0F, -150.0F, -225.0F, this.color, this.color2));
+        AbstractDungeon.effectsQueue.add(new AnimatedSlashEffect(this.x + 35.0F, this.y - 35.0F, -150.0F, -150.0F, -225.0F, this.color, this.color2));
+        this.isDone = true;
+    }
+
+    public void render(SpriteBatch sb) {}
+
+    public void dispose() {}
+}
