8273b9f9471215bb6c19b48a77cfdbb995b9ad96
12138 <drelf2018@outlook.com>
2025-08-23 00:58:27 +0800

    fix: 修复了那咋了使用非游戏内随机数的问题

--- src/main/java/nana7mimod/cards/SoWhat.java
+++ src/main/java/nana7mimod/cards/SoWhat.java
@@ -5,9 +5,9 @@ import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import nana7mimod.helpers.ModHelper;
-import java.lang.Math;
 
 public class SoWhat extends Base {
     public static final String ID = ModHelper.id(SoWhat.class);
@@ -28,7 +28,7 @@ public class SoWhat extends Base {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        returnToHand = 100 * Math.random() < magicNumber;
+        returnToHand = AbstractDungeon.cardRandomRng.random(99) < magicNumber;
         addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
     }
 }
