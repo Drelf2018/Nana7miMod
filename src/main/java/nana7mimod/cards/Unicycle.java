0fb524e40048b8ab12488d5f7600106086c84168
12138 <drelf2018@outlook.com>
2025-08-26 12:59:03 +0800

    feat: 独轮车

---
+++ src/main/java/nana7mimod/cards/Unicycle.java
@@ -0,0 +1,50 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.ATFieldPower;
+
+public class Unicycle extends Base {
+    public static final String ID = ModHelper.id(Unicycle.class);
+
+    public Unicycle() {
+        super(ID, CardCost.CX, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 6;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(3);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int effect = EnergyPanel.totalCount;
+        if (energyOnUse != -1) {
+            effect = energyOnUse;
+        }
+
+        if (p.hasRelic("Chemical X")) {
+            effect += 2;
+            p.getRelic("Chemical X").flash();
+        }
+
+        if (effect > 0) {
+            for (int i = 0; i < effect; ++i) {
+                addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.BLUNT_LIGHT));
+            }
+
+            if (!this.freeToPlayOnce) {
+                p.energy.use(EnergyPanel.totalCount);
+            }
+        }
+
+        ATFieldPower.setAmount(p, -effect);
+    }
+}
