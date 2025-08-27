9b3cc55bbe3e4b509ee5c52aba62d87bdf9e5c4e
12138 <drelf2018@outlook.com>
2025-08-28 02:03:24 +0800

    feat: 兄弟们有神！

---
+++ src/main/java/nana7mimod/cards/God.java
@@ -0,0 +1,33 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.MantraPower;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.ATFieldPower;
+
+public class God extends Base {
+    public static final String ID = ModHelper.id(God.class);
+
+    public God() {
+        super(ID, CardCost.C2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(cost - 1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        ATFieldPower.setAmount(p, amount -> {
+            if (amount <= 0)
+                return amount;
+            for (int i = 0; i < amount; ++i)
+                addToBot(new ApplyPowerAction(p, p, new MantraPower(p, 1)));
+            return 0;
+        });
+    }
+}
