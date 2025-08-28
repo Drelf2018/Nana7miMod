5e2a528c745caaef2d44ea096da9716b012e4edb
12138 <drelf2018@outlook.com>
2025-08-28 22:22:04 +0800

    feat: 好呆啊！

---
+++ src/main/java/nana7mimod/cards/Silly.java
@@ -0,0 +1,30 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ConfusionPower;
+import nana7mimod.helpers.ModHelper;
+
+public class Silly extends Base {
+    public static final String ID = ModHelper.id(Silly.class);
+
+    public Silly() {
+        super(ID, CardCost.C1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 3;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!p.hasPower(ConfusionPower.POWER_ID))
+            addToBot(new ApplyPowerAction(p, p, new ConfusionPower(p)));
+        addToBot(new DrawCardAction(p, magicNumber));
+    }
+}
