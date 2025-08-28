6a089f791683209e09abc4180f7b86569536e8fc
12138 <drelf2018@outlook.com>
2025-08-28 22:35:31 +0800

    fix: 优化怒火中烧

--- src/main/java/nana7mimod/cards/Furious.java
+++ src/main/java/nana7mimod/cards/Furious.java
@@ -1,12 +1,13 @@
 package nana7mimod.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import nana7mimod.actions.ExhaustAllAction;
 import nana7mimod.helpers.ModHelper;
 import nana7mimod.powers.ATFieldPower;
+import com.megacrit.cardcrawl.core.Settings;
 
 public class Furious extends Base {
     public static final String ID = ModHelper.id(Furious.class);
@@ -25,15 +26,22 @@ public class Furious extends Base {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int exhaustCount = 0;
-        for (AbstractCard c : p.hand.group) {
-            if (c.type == CardType.ATTACK) {
-                exhaustCount++;
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                int exhaustCount = p.hand.size();
+                if (exhaustCount > 0) {
+                    addToTop(new ApplyPowerAction(p, p, new ATFieldPower(p, exhaustCount * magicNumber)));
+                }
+                for (int i = 0; i < exhaustCount; ++i) {
+                    if (Settings.FAST_MODE) {
+                        addToTop((AbstractGameAction) new ExhaustAction(1, true, true, false, Settings.ACTION_DUR_XFAST));
+                    } else {
+                        addToTop((AbstractGameAction) new ExhaustAction(1, true, true));
+                    }
+                }
+                isDone = true;
             }
-        }
-        if (exhaustCount != 0) {
-            addToBot(new ExhaustAllAction(p.hand, CardType.ATTACK));
-            addToBot(new ApplyPowerAction(p, p, new ATFieldPower(p, exhaustCount * magicNumber)));
-        }
+        });
     }
 }
