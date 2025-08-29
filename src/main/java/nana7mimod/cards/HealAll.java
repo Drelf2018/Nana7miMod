5471686035e4d990dcc4bc6d789548846d3b2bc9
12138 <drelf2018@outlook.com>
2025-08-29 23:04:18 +0800

    feat: 桃园结义

---
+++ src/main/java/nana7mimod/cards/HealAll.java
@@ -0,0 +1,32 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+
+public class HealAll extends Base {
+    public static final String ID = ModHelper.id(HealAll.class);
+
+    public HealAll() {
+        super(ID, CardCost.C0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL);
+        this.magicNumber = this.baseMagicNumber = 5;
+        this.exhaust = true;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(2);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new HealAction(p, p, magicNumber));
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+            if (!mo.isDeadOrEscaped())
+                addToBot(new HealAction(mo, p, magicNumber));
+        }
+    }
+}
