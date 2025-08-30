fd582318c7cbb9fb305fa45d28ea5f511751f965
12138 <drelf2018@outlook.com>
2025-08-30 18:36:05 +0800

    feat: 请你喝

---
+++ src/main/java/nana7mimod/cards/BuyYou.java
@@ -0,0 +1,32 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import nana7mimod.helpers.ModHelper;
+
+public class BuyYou extends Base {
+    public static final String ID = ModHelper.id(BuyYou.class);
+
+    public BuyYou() {
+        super(ID, CardCost.C0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 2;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDescription();
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (upgraded)
+            addToBot(new GainEnergyAction(3));
+        else
+            addToBot(new GainEnergyAction(2));
+        addToBot(new ApplyPowerAction(p, p, new FrailPower(p, magicNumber, false)));
+    }
+}
