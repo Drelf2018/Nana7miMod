7a5a625666859e80d26eaf4534d2898251ce359e
12138 <drelf2018@outlook.com>
2025-08-31 15:41:41 +0800

    fix: 使用官方保留卡牌方法

--- src/main/java/nana7mimod/cards/RestartStreaming.java
+++ src/main/java/nana7mimod/cards/RestartStreaming.java
@@ -2,10 +2,10 @@ package nana7mimod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ConservePower;
+import com.megacrit.cardcrawl.powers.EquilibriumPower;
 import nana7mimod.helpers.ModHelper;
 
 public class RestartStreaming extends Base {
@@ -23,10 +23,7 @@ public class RestartStreaming extends Base {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (AbstractCard c : p.hand.group) {
-            if (!c.isEthereal && c != this)
-                c.retain = true;
-        }
+        addToBot(new ApplyPowerAction(p, p, new EquilibriumPower(p, 1)));
         addToBot(new ApplyPowerAction(p, p, new ConservePower(p, 1)));
         addToBot(new PressEndTurnButtonAction());
     }
