af9b620e2e37afd328b5b7d384d67b696ce5bdfb
12138 <drelf2018@outlook.com>
2025-08-31 18:29:31 +0800

    fix: 优化我给你钱别那么闲特效

--- src/main/java/nana7mimod/cards/Idle.java
+++ src/main/java/nana7mimod/cards/Idle.java
@@ -5,7 +5,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.vfx.GainPennyEffect;
+import nana7mimod.effects.LosePennyEffect;
 import nana7mimod.helpers.ModHelper;
 
 public class Idle extends Base {
@@ -15,6 +15,25 @@ public class Idle extends Base {
         super(ID, CardCost.C0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
         this.magicNumber = this.baseMagicNumber = 10;
         this.exhaust = true;
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        boolean canUse = super.canUse(p, m);
+        if (!canUse)
+            return false;
+
+        int totalGold = 0;
+        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters)
+            if (!mo.isDeadOrEscaped())
+                totalGold += magicNumber;
+
+        if (totalGold > p.gold) {
+            canUse = false;
+            cantUseMessage = strings(ID).EXTENDED_DESCRIPTION[0];
+        }
+
+        return canUse;
     }
 
     public void upgrade() {
@@ -28,9 +47,8 @@ public class Idle extends Base {
         for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
             if (!mo.isDeadOrEscaped()) {
                 p.loseGold(magicNumber);
-                for (int i = 0; i < magicNumber; ++i) {
-                    AbstractDungeon.effectList.add(new GainPennyEffect(mo, p.hb.cX, p.hb.cY, mo.hb.cX, mo.hb.cY, false));
-                }
+                for (int i = 0; i < magicNumber; ++i)
+                    AbstractDungeon.effectList.add(new LosePennyEffect(mo, p.hb.cX, p.hb.cY, mo.hb.cX, mo.hb.cY, true));
                 addToBot(new ApplyPowerAction(mo, p, new VulnerablePower(mo, 99, false)));
             }
         }
