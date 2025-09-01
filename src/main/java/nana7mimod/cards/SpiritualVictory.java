288e67a274670e28f37b22d83cc49f7fe972b61d
12138 <drelf2018@outlook.com>
2025-09-01 22:16:17 +0800

    feat: 精神胜利

---
+++ src/main/java/nana7mimod/cards/SpiritualVictory.java
@@ -0,0 +1,34 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import nana7mimod.helpers.ModHelper;
+
+public class SpiritualVictory extends Base {
+    public static final String ID = ModHelper.id(SpiritualVictory.class);
+
+    public SpiritualVictory() {
+        super(ID, CardCost.C1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 4;
+        this.magicNumber = this.baseMagicNumber = 1;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(3);
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.SLASH_DIAGONAL));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+    }
+}
