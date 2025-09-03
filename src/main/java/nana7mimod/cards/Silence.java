0148e360292f395fcbf4f22542320de16ef5d179
12138 <drelf2018@outlook.com>
2025-09-03 12:54:39 +0800

    remove: 移除了不必要的禁言动作

--- src/main/java/nana7mimod/cards/Silence.java
+++ src/main/java/nana7mimod/cards/Silence.java
@@ -1,9 +1,15 @@
 package nana7mimod.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 import nana7mimod.helpers.ModHelper;
-import nana7mimod.actions.SilenceAction;
+import nana7mimod.powers.SilencePower;
 
 public class Silence extends Base {
     public static final String ID = ModHelper.id(Silence.class);
@@ -22,6 +28,17 @@ public class Silence extends Base {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new SilenceAction(m));
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                if (m.getIntentBaseDmg() >= 0)
+                    addToBot(new ApplyPowerAction(m, m, new SilencePower(m, 1)));
+                else {
+                    UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("OpeningAction");
+                    AbstractDungeon.effectList.add(new ThoughtBubble(p.dialogX, p.dialogY, 3.0F, uiStrings.TEXT[0], true));
+                }
+                isDone = true;
+            }
+        });
     }
 }
