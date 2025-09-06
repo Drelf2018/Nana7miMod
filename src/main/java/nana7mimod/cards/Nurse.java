6cad43bc1e24b505e04e7ba62255905da8a80440
12138 <drelf2018@outlook.com>
2025-09-06 12:55:03 +0800

    feat: 打给你了

---
+++ src/main/java/nana7mimod/cards/Nurse.java
@@ -0,0 +1,33 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+
+public class Nurse extends Base {
+    public static final String ID = ModHelper.id(Nurse.class);
+
+    public Nurse() {
+        super(ID, CardCost.C2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 10;
+        this.magicNumber = this.baseMagicNumber = 4;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(3);
+            upgradeMagicNumber(2);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
+        addToBot(new HealAction(p, p, magicNumber));
+    }
+}
