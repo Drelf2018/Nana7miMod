814375049b08bc1f30ffc63fe7f539194ded3c0f
12138 <drelf2018@outlook.com>
2025-09-06 13:31:29 +0800

    feat: 滴泪

---
+++ src/main/java/nana7mimod/cards/Overdose.java
@@ -0,0 +1,39 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.cards.status.Wound;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
+import nana7mimod.helpers.ModHelper;
+
+public class Overdose extends Base {
+    public static final String ID = ModHelper.id(Overdose.class);
+
+    public Overdose() {
+        super(ID, CardCost.C1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        this.damage = this.baseDamage = 12;
+        this.magicNumber = this.baseMagicNumber = 2;
+        this.isMultiDamage = true;
+        this.cardsToPreview = new Wound();
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(5);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster n) {
+        addToBot(new SFXAction("ATTACK_HEAVY"));
+        addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
+        addToBot(new DamageAllEnemiesAction(p, multiDamage, DamageType.NORMAL, AbstractGameAction.AttackEffect.NONE));
+        addToBot(new MakeTempCardInDrawPileAction(new Wound(), magicNumber, true, true));
+    }
+}
