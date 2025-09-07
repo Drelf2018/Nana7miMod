2f000e41cba36208b518cdaae9df28f4a1b5fb54
12138 <drelf2018@outlook.com>
2025-09-07 11:26:05 +0800

    fix: 增强好呆啊！

--- src/main/java/nana7mimod/cards/Silly.java
+++ src/main/java/nana7mimod/cards/Silly.java
@@ -2,6 +2,7 @@ package nana7mimod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ConfusionPower;
@@ -11,7 +12,7 @@ public class Silly extends Base {
     public static final String ID = ModHelper.id(Silly.class);
 
     public Silly() {
-        super(ID, CardCost.C1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        super(ID, CardCost.C0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         this.magicNumber = this.baseMagicNumber = 3;
     }
 
@@ -24,7 +25,13 @@ public class Silly extends Base {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (!p.hasPower(ConfusionPower.POWER_ID))
-            addToBot(new ApplyPowerAction(p, p, new ConfusionPower(p)));
+            addToBot(new ApplyPowerAction(p, p, new ConfusionPower(p) {
+                @Override
+                public void atEndOfTurn(boolean isPlayer) {
+                    if (isPlayer)
+                        addToBot(new ReducePowerAction(owner, owner, this, 1));
+                }
+            }));
         addToBot(new DrawCardAction(p, magicNumber));
     }
 }
