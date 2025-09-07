7cff28107eef26f91610af826b88acc9ba06ea4a
12138 <drelf2018@outlook.com>
2025-09-07 22:42:46 +0800

    feat: 头显就是苹果

---
+++ src/main/java/nana7mimod/cards/Vision.java
@@ -0,0 +1,34 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+
+public class Vision extends Base {
+    public static final String ID = ModHelper.id(Vision.class);
+
+    public Vision() {
+        super(ID, CardCost.C1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 8;
+        this.magicNumber = this.baseMagicNumber = 1;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(2);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.SLASH_DIAGONAL));
+        addToBot(new ScryAction(magicNumber));
+        addToBot(new DrawCardAction(magicNumber));
+    }
+}
