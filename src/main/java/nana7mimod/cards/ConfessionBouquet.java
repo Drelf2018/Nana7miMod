556b87f948e57fcf8bfb9b25e39c7395c21b849e
12138 <drelf2018@outlook.com>
2025-09-12 01:08:36 +0800

    feat: 告白花束

---
+++ src/main/java/nana7mimod/cards/ConfessionBouquet.java
@@ -0,0 +1,28 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.BouquetPower;
+
+public class ConfessionBouquet extends Base {
+    public static final String ID = ModHelper.id(ConfessionBouquet.class);
+
+    public ConfessionBouquet() {
+        super(ID, CardCost.C1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 1;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+            changeCardImage("ConfessionBouquet_2025");
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new BouquetPower(p, magicNumber)));
+    }
+}
---
+++ src/main/java/nana7mimod/powers/BouquetPower.java
@@ -0,0 +1,49 @@
+package nana7mimod.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import nana7mimod.helpers.ModHelper;
+
+public class BouquetPower extends AbstractPower {
+    public static final String POWER_ID = ModHelper.id(BouquetPower.class);
+
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public BouquetPower(AbstractCreature owner, int amount) {
+        this.name = powerStrings.NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+
+        updateDescription();
+        loadRegion("time"); // 换成😭
+    }
+
+    // 群体入脑
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            int applyCount = 0;
+            for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters)
+                if (!mo.hasPower(InfatuationPower.POWER_ID)) {
+                    applyCount++;
+                    addToBot(new ApplyPowerAction(mo, owner, new InfatuationPower(mo, amount)));
+                }
+            if (applyCount != 0)
+                flashWithoutSound();
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+}
