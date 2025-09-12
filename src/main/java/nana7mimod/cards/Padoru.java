64057cff1582cdbe06cc83d2ff8741be1981490d
12138 <drelf2018@outlook.com>
2025-09-12 22:00:34 +0800

    feat: 帕多路

---
+++ src/main/java/nana7mimod/cards/Padoru.java
@@ -0,0 +1,27 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.PadoruPower;
+
+public class Padoru extends Base {
+    public static final String ID = ModHelper.id(Padoru.class);
+
+    public Padoru() {
+        super(ID, CardCost.C2, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 1;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(cost - 1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new PadoruPower(p, magicNumber)));
+    }
+}
---
+++ src/main/java/nana7mimod/powers/PadoruPower.java
@@ -0,0 +1,46 @@
+package nana7mimod.powers;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardRarity;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import nana7mimod.helpers.ModHelper;
+
+public class PadoruPower extends AbstractPower {
+    public static final String POWER_ID = ModHelper.id(PadoruPower.class);
+
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public PadoruPower(AbstractCreature owner, int amount) {
+        this.name = powerStrings.NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+
+        updateDescription();
+        loadRegion("time"); // 换成😭
+    }
+
+    public void atStartOfTurn() {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            flash();
+            for (int i = 0; i < this.amount; i++) {
+                AbstractCard c = AbstractDungeon.getCardFromPool(CardRarity.UNCOMMON, CardType.SKILL, true).makeCopy();
+                addToBot(new MakeTempCardInHandAction(c, 1, false));
+            }
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+}
