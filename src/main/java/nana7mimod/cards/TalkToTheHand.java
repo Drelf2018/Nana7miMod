8dd9b00cffdf943e3784c49400e565316c714d77
12138 <drelf2018@outlook.com>
2025-09-16 23:10:05 +0800

    remove: 移除了拒之以手特效

--- src/main/java/nana7mimod/cards/TalkToTheHand.java
+++ src/main/java/nana7mimod/cards/TalkToTheHand.java
@@ -1,15 +1,12 @@
 package nana7mimod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.WeightyImpactEffect;
 import nana7mimod.helpers.ModHelper;
 import nana7mimod.powers.BetrayPower;
 
@@ -30,9 +27,7 @@ public class TalkToTheHand extends Base {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
-        addToBot(new WaitAction(0.8F));
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.NONE));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.BLUNT_HEAVY));
         addToBot(new ApplyPowerAction(m, p, new BetrayPower(m, magicNumber, false)));
     }
 }
