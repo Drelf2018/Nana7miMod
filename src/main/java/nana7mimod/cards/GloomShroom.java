9a2dca142be4f15f566509252a7d3e3cef805b6f
12138 <drelf2018@outlook.com>
2025-09-18 02:50:15 +0800

    feat: 经典忧郁菇

---
+++ src/main/java/nana7mimod/cards/GloomShroom.java
@@ -0,0 +1,33 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+
+public class GloomShroom extends Base {
+    public static final String ID = ModHelper.id(GloomShroom.class);
+
+    public GloomShroom() {
+        super(ID, CardCost.C2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = 1;
+        this.magicNumber = this.baseMagicNumber = 6;
+        this.isEthereal = true;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < magicNumber; ++i) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL), AttackEffect.SLASH_HORIZONTAL));
+        }
+    }
+}
