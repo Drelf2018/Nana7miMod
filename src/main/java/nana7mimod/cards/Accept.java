bd0a661029f3550f520baed26c4052dfe1368ed2
12138 <drelf2018@outlook.com>
2025-09-21 03:14:13 +0800

    feat: 为和解添加使用提示特效

--- src/main/java/nana7mimod/cards/Accept.java
+++ src/main/java/nana7mimod/cards/Accept.java
@@ -1,8 +1,11 @@
 package nana7mimod.cards;
 
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import nana7mimod.helpers.ModHelper;
 import nana7mimod.powers.ATFieldPower;
 
@@ -11,6 +14,15 @@ public class Accept extends Base {
 
     public Accept() {
         super(ID, CardCost.C1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        AbstractPower p = ATFieldPower.from(AbstractDungeon.player);
+        if (p != null && p.amount > cost)
+            glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        else
+            glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
     }
 
     public void upgrade() {
