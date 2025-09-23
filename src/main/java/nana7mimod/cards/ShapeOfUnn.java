5ad1cb109d034d392fd0d15a9fcf029357813761
12138 <drelf2018@outlook.com>
2025-09-23 22:34:00 +0800

    feat: 乌恩之形

---
+++ src/main/java/nana7mimod/cards/ShapeOfUnn.java
@@ -0,0 +1,27 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.UnnPower;
+
+public class ShapeOfUnn extends Base {
+    public static final String ID = ModHelper.id(ShapeOfUnn.class);
+
+    public ShapeOfUnn() {
+        super(ID, CardCost.C2, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 5;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(cost - 1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new UnnPower(p, magicNumber)));
+    }
+}
---
+++ src/main/java/nana7mimod/powers/UnnPower.java
@@ -0,0 +1,52 @@
+package nana7mimod.powers;
+
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import nana7mimod.helpers.ModHelper;
+
+public class UnnPower extends AbstractPower {
+    public static final String POWER_ID = ModHelper.id(UnnPower.class);
+
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public UnnPower(AbstractCreature owner, int amount) {
+        this.name = powerStrings.NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+
+        updateDescription();
+        loadRegion("time"); // 换成😭
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            boolean playedAttackCard = false;
+            for (AbstractCard c : AbstractDungeon.actionManager.cardsPlayedThisTurn)
+                if (c.type == CardType.ATTACK) {
+                    playedAttackCard = true;
+                    break;
+                }
+
+            if (!playedAttackCard) {
+                flashWithoutSound();
+                addToTop(new HealAction(owner, owner, amount));
+            }
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+}
