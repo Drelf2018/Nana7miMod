25e858c7a432a0407f7f80af340521973c49a292
12138 <drelf2018@outlook.com>
2025-09-29 00:51:22 +0800

    feat: 新增脆鲨启动
    最后一张牌终于写完了

---
+++ src/main/java/nana7mimod/cards/CuishaLetsGO.java
@@ -0,0 +1,46 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.defect.IncreaseMiscAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.BiteEffect;
+import nana7mimod.helpers.CharacterHelper.Nana7mi;
+import nana7mimod.helpers.ModHelper;
+
+public class CuishaLetsGO extends Base {
+	public static final String ID = ModHelper.id(CuishaLetsGO.class);
+
+	public CuishaLetsGO() {
+		super(ID, CardCost.C2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+		this.misc = 4;
+		this.damage = this.baseDamage = this.misc;
+		this.magicNumber = this.baseMagicNumber = 4;
+		this.exhaust = true;
+	}
+
+	@Override
+	public void applyPowers() {
+		baseDamage = misc;
+		super.applyPowers();
+		initializeDescription();
+	}
+
+	public void upgrade() {
+		if (!upgraded) {
+			upgradeName();
+			upgradeMagicNumber(2);
+		}
+	}
+
+	public void use(AbstractPlayer p, AbstractMonster m) {
+		addToBot(new IncreaseMiscAction(uuid, misc, magicNumber));
+		if (m != null)
+			addToBot(new VFXAction(new BiteEffect(m.hb.cX, m.hb.cY - 40.0F * Settings.scale, Nana7mi.COLOR.cpy()), 0.3F));
+		addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageType.NORMAL)));
+	}
+}
