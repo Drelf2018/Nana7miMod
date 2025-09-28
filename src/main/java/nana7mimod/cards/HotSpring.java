6719adc9b6700fdf873ae270185f902cd01c7ee9
12138 <drelf2018@outlook.com>
2025-09-28 23:53:06 +0800

    feat: 一起泡温泉

---
+++ src/main/java/nana7mimod/cards/HotSpring.java
@@ -0,0 +1,27 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.HotSpringPower;
+
+public class HotSpring extends Base {
+    public static final String ID = ModHelper.id(HotSpring.class);
+
+    public HotSpring() {
+        super(ID, CardCost.C1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 3;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new HotSpringPower(p, magicNumber)));
+    }
+}
---
+++ src/main/java/nana7mimod/powers/HotSpringPower.java
@@ -0,0 +1,49 @@
+package nana7mimod.powers;
+
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import nana7mimod.helpers.ModHelper;
+
+public class HotSpringPower extends AbstractPower {
+    public static final String POWER_ID = ModHelper.id(HotSpringPower.class);
+
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public HotSpringPower(AbstractCreature owner, int amount) {
+        this.name = powerStrings.NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+
+        updateDescription();
+        loadRegion("like_water");
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.type == AbstractCard.CardType.ATTACK) {
+            flashWithoutSound();
+            addToBot(new HealAction(AbstractDungeon.player, AbstractDungeon.player, amount));
+        }
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+}
