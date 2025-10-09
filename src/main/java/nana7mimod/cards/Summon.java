e755005b60daaa049045800d38f9bc1b4a300c39
12138 <drelf2018@outlook.com>
2025-10-10 01:16:34 +0800

    feat: 召唤术式

---
+++ src/main/java/nana7mimod/cards/Summon.java
@@ -0,0 +1,29 @@
+package nana7mimod.cards;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nana7mimod.helpers.ModHelper;
+import nana7mimod.powers.ATFieldPower;
+
+public class Summon extends Base {
+    public static final String ID = ModHelper.id(Summon.class);
+
+    public Summon() {
+        super(ID, CardCost.C1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.magicNumber = this.baseMagicNumber = 3;
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        ATFieldPower power = ATFieldPower.from(p);
+        int extraDraws = power != null && power.amount > 0 ? power.amount : 0;
+        addToBot(new DrawCardAction(p, 2 + Math.min(extraDraws, magicNumber)));
+    }
+}
